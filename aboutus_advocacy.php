d2a342781b53e2a5ddaf6232177dcaf462df9c0a
Rizal Purwosaputro <rizal.purwosaputro@gmail.com>
2015-04-29 14:17:41 +0700

    Publish 2
    coba publish

---
+++ aboutus_advocacy.php
@@ -0,0 +1,109 @@
+<h2><img src="images/certificate.png" width="30" height="30"> Advocacy
+  <hr color="#EEEEEE" width="90%" align="left"></h2>
+<table>  
+    <tr>
+    <td>
+        <ol>
+   		  <li><font  face="Arial, Helvetica, sans-serif">
+       	  Akte Pendirian (Anggaran Dasar) Yayasan Dompet Sosial Ummul Quro (DSUQ) No mor 31 tanggal 12 Juli 2001 yang dibuat di hadapan Notaris Dr. Wiratni Ahmadi.<br>
+          </font></li><br>
+   		  <li><font  face="Arial, Helvetica, sans-serif">
+          Keputusan Menteri Agama RI No 157/tahun 2003 tentang Pengukuhan Yayasan DSUQ menjadi Lembaga Amil Zakat .<br>
+          </font></li><br>
+          <li><font  face="Arial, Helvetica, sans-serif">
+   	      Akta Pernyataan Keputusan Rapat Pembina Yayasan Zakat Online Indonesia Nomor 12 tanggal 15 Juli 2005 yang dibuat di hadapan Notaris Irma Rachmawati, SH.
+          <br><br>
+          <ul>
+          <li type="disc">
+          Perubahan nama dari Yayasan Dompet Sosial Ummul Quro menjadi Yayasan Zakat Online Indonesia;<br>
+          </li>
+          <li type="disc">
+          Penetapan Susunan Pengurus, Pembina dan Pengawas Yayasan.<br>
+		  </li>
+          <li type="disc">
+          Penyesuaian Anggaran Dasar Yayasan Zakat Online Indonesia dengan Undang-undang Yayasan Nomor 16 tahun 2001.<br>
+          </li>
+          </ul>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+   	      Akta Perubahan Yayasan Zakat Online Indonesia Nomor 17 tanggal 25 Oktober 2005 yang dibuat di hadapan Notaris Irma Rachmawati.<br>
+          <br>
+          <ul>
+          <li type="disc">
+          Penyesuaian Anggaran Dasar Yayasan Zakat Online Indonesia dengan Undang-undang Yayasan Nomor 28 tahun 2004.<br>
+          </li>
+          </ul>
+          </font>
+          </li>
+          <br>
+          <li><font  face="Arial, Helvetica, sans-serif">
+		  Keputusan Menteri Hukum dan HAM Republik Indonesua Nomor C-1490.HT.01.02/Th.2006 tanggal 25 Juli 2006 tentang Pengesahan 
+          Akta Pendirian Yayasan Zakat Online Indonesia.<br>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+       	  Berita Negara Republik Indonesia Nomor : 68, tertanggal 22 Agustus 2008, Tambahan Berita Negara Nomor : 1071.<br>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+       	  Akta Pernyataan Keputusan Pembina Yayasan Zakat Online Indonesia Nomor 11 tanggal 11 Desember 2008 yang dibuat dihadapan Notaris Irma Rachmawati,SH.<br>
+          <br>
+          <ul>
+          <li type="disc">
+          Penetapan Susunan Pengurus, Pembina dan Pengawas Yayasan.<br>
+          </li>
+          </ul>
+          </font>
+          </li>
+          <br>
+          <li><font  face="Arial, Helvetica, sans-serif">
+       	  Surat Keterangan Tercatat dari Dirjen Administrasi Hukum Umum Depkumham RI Nomor AHU-AH.01.08-872 tertanggal 24 Desember 2008
+          tentang Pencatatan Akta Pernyataan Keputusan Pembina Yayasan Zakat Online Indonesia Nomor 11 tanggal 11 Desember 2008.<br>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+       	  Keputusan Menteri Agama Republik Indonesia Nomor 42 Tahun 2007 tentang Pengukuhan Yayasan Zakat Online Indonesia sebagai Lembaga Amil Zakat.<br>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+       	  SK Dinas Sosial Propinsi Jawa Barat Nomor 062/1394/PRKS/2006 tanggal 16 Juni 2006 tentang Penetapan Yayasan Zakat Online Indonesia sebagai Organisasi Sosial.<br>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+   	      Surat Keterangan Terdaftar di Dinas Sosial Pemerintah Kota Bandung :<br>
+          <br>
+          <ul>
+          <li type="disc">
+          Tahun 2006 : Nomor 062/64-Kansos tanggal 15 Maret 2006.<br>
+          </li>
+          <li type="disc">
+          Tahun 2007 : Nomor 062/531-Kansos tanggal 19 April 2007.<br>
+		  </li>
+          <li type="disc">
+          Tahun 2008 : Nomor 062/1017-Dinsos tanggal 26 Nopember 2008<br>
+          </li>
+          </ul>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+		  NPWP : 02.083.957.7.424.000.<br>
+          </font>
+          <br>
+          </li>
+          <li><font  face="Arial, Helvetica, sans-serif">
+          Surat Keterangan Domisili Yayasan Zakat Online Indonesia Nomor : 05/DP/I/2006 tanggal 26 Januari 2006.<br>
+          </font>
+          <br>
+          </li>
+      </ol>
+    </td>
+  </tr>
+</table>
